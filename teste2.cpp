d5cc07422868476b1bd2b49d0046f3189c558ee9
Emanuel G Santana <emanuel.gui.santana@gmail.com>
2024-05-06 10:39:38 -0300

    primeiro commit

---
+++ teste2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <random>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+string gerarString(int comprimento) {
+    string s;
+    for (int i = 0; i < comprimento; ++i) {
+        char c = rand() % 26 + 'a'; // Gera um caractere minúsculo aleatório
+        s.push_back(c);
+    }
+    return s;
+}
+
+int main(){
+
+  int chave[1000];
+  for(int i = 0; i < 1000; i++){
+    chave[i] = rand();
+  }
+
+  int dado1[1000];
+  for(int i = 0; i < 1000; i++){
+    dado1[i] = i;
+  }
+  srand(time(nullptr)); 
+
+  ofstream outfile ("teste.txt");
+
+  for(int i = 0; i < 1000; i++){
+  outfile << chave[i]<< " , "<<dado1[i]<< " , " << gerarString(1000) << endl;
+  }
+}
+
