0bf64149a099b73d3ab84648c43ff6acd0d7764d
Emanuel G Santana <emanuel.gui.santana@gmail.com>
2024-05-06 23:25:13 -0300

    comentando codigo

--- gerador.cpp
+++ gerador.cpp
@@ -1,3 +1,4 @@
+//Bibliotecas utilizadas
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -8,6 +9,7 @@
 
 using namespace std;
 
+//Função que gera Strings aleatorias de comprimentos definidos pelo usuário
 string gerarString(int comprimento) {
     string s;
     for (int i = 0; i < comprimento; ++i) {
@@ -19,26 +21,32 @@ string gerarString(int comprimento) {
 
 int main(){
 
+  //Recebe a quantidade de registros que o usuário quer
   int x = 0;
   cout << "Digite a quantidade de registros que voce quer: ";
   cin >> x;
 
+  //Forma de gerar números aleatórios
   random_device rd;
   mt19937 gen(rd());
   uniform_int_distribution<int> dis(0, 1000000);
 
+  //Preenche o array de chaves com números aleatórios para cada posição do array
   int chave[x];
   for(int i = 0; i < x; i++){
     chave[i] = dis(gen);
   }
 
+  //Gera números sequenciais para o dado1
   int dado1[x];
   for(int i = 0; i < x; i++){
     dado1[i] = i;
   }
 
+  //Cria um arquivo .txt
   ofstream outfile ("teste.txt");
 
+  //Preenche o arquivo .txt com uma chave aleatória, o numero de ordem do registro e uma string aleatória respectivamente
   for(int i = 0; i < x; i++){
   outfile << chave[i]<< " , "<<dado1[i]<< " , " << gerarString(1000) << endl;
   }
